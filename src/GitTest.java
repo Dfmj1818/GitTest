02471413935ceff66a4a9a2fda4c102c0292e088
Dfmj1818 <danieljimenezgg@gmail.com>
2023-08-17 11:01:02 -0500

    for added

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,5 +4,13 @@ public class GitTest {
 	public static void main(String[]args) {
 		System.out.println("GitTest!!!");
 		System.out.println("Prueba numero 2");
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
+		
+
+
+
+
 	}
 }
